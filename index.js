57cdb2e20e57afbc9f9e094164f0afa32074aebb
EDUARDO HENRIQUE <72581333+eduardohbarbosa@users.noreply.github.com>
2024-05-22 11:08:30 -0300

    updade projeto

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+let nome = 'Luffy';
+let xp = 1100000;
 
-let nome = '';
-let xp = 0;
-
-readline.question('Qual é seu nome?', name => {
-    nome = name
-    readline.close()
-});
-
-readline.question('Quanta XP  o heroi ?', exp => {
-    xp = exp
-    readline.close
-});
-
-switch (xp) {
-    case xp <= 1000:
-        console.log(`O Herói de nome ${nome} está no nível de Ferro`)
-        break;
-    case xp <= 2000:
-        console.log(`O Herói de nome ${nome} está no nível de Bronze`)
-        break;
-    case xp <= 5000:
-        console.log(`O Herói de nome ${nome} está no nível de Prata`)
-        break;
-    case xp <= 7000:
-        console.log(`O Herói de nome ${nome} está no nível de Ouro`)
-        break;
-    case xp <= 8000:
-        console.log(`O Herói de nome ${nome} está no nível de Platina`)
-        break;
-    case xp <= 9000:
-        console.log(`O Herói de nome ${nome} está no nível de Ascendente`)
-        break;
-    case xp <= 10000:
-        console.log(`O Herói de nome ${nome} está no nível de Imortal`)
-        break;
-    case xp > 10000:
-        console.log(`O Herói de nome ${nome} está no nível de Radiante`)
-        break;
-    default:
-        break;
+if (xp <= 1000){
+  console.log(`O Herói de nome ${nome} está no nível de Ferro`)
+}else if(xp <= 2000){
+  console.log(`O Herói de nome ${nome} está no nível de Bronze`)
+}else if(xp <= 5000){
+  console.log(`O Herói de nome ${nome} está no nível de Prata`)
+}else if(xp <= 7000){
+  console.log(`O Herói de nome ${nome} está no nível de Ouro`)
+}else if(xp <= 8000){
+  console.log(`O Herói de nome ${nome} está no nível de Platina`)
+}else if(xp <= 9000){
+  console.log(`O Herói de nome ${nome} está no nível de Ascendente`)
+}else if(xp <= 10000){
+  console.log(`O Herói de nome ${nome} está no nível de Imortal`)
+}else{
+  console.log(`O Herói de nome ${nome} está no nível de Radiante`)
 }
